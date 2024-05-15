65e8e15adea04b181d42499dc8e5ae619daf2414
dan <daniel_tzonkov@abv.bg>
2024-05-15 15:46:27 +0300

    15052024

--- src/main/java/_01/WizardDeposits.java
+++ src/main/java/_01/WizardDeposits.java
@@ -1,10 +1,22 @@
 package _01;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "wizard_deposits")
+@Table(name = "_01_wizard_deposits")
 public class WizardDeposits {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", length = 60, nullable = false)
+    private String lastName;
+
+    @Column(length = 1000)
+    private String notes;
+
+    private int age;
 }
