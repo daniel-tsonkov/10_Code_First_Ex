cee2369cfa1a636ad9fa643714d6eaed391bf5c4
dan <daniel_tzonkov@abv.bg>
2024-05-16 10:07:00 +0300

    16052024

---
+++ src/main/java/_02/entities/Customer.java
@@ -0,0 +1,5 @@
+package _02.entities;
+
+public class Customer {
+
+}
--- src/main/java/_02/entities/Product.java
+++ src/main/java/_02/entities/Product.java
@@ -23,4 +23,36 @@ public class Product {
         this.quantity = quantity;
         this.price = price;
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
