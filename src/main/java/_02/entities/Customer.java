ac3a5df28ce72f4eeeb30a202c356e99e17a0607
dan <daniel_tzonkov@abv.bg>
2024-05-16 10:21:37 +0300

    16052024

--- src/main/java/_02/entities/Customer.java
+++ src/main/java/_02/entities/Customer.java
@@ -1,5 +1,17 @@
 package _02.entities;
 
-public class Customer {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "_02_customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String email;
+
+    private String creditCardNumber;
 }
--- src/main/java/_02/entities/Product.java
+++ src/main/java/_02/entities/Product.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "products")
+@Table(name = "_02_products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
