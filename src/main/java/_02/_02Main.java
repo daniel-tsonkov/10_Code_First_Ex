ec29a1c29a5d435fca9e5dfd4aff56262e62f410
dan <daniel_tzonkov@abv.bg>
2024-05-27 11:02:42 +0300

    27052024

--- src/main/java/_02/_02Main.java
+++ src/main/java/_02/_02Main.java
@@ -1,8 +1,14 @@
 package _02;
 
+import _02.entities.Customer;
+import _02.entities.Product;
+import _02.entities.Sale;
+import _02.entities.StoreLoacation;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.math.BigDecimal;
 
 public class _02Main
 {
@@ -13,7 +19,15 @@ public class _02Main
         EntityManager entityManager = factory.createEntityManager();
         entityManager.getTransaction().begin();
 
+        Product product = new Product("product", 123, BigDecimal.TEN);
+        Customer customer = new Customer("cName", "asd@asd.asd", "12345");
+        StoreLoacation loacation = new StoreLoacation("ssstore");
+        Sale sale = new Sale(product, customer, loacation);
 
+        entityManager.persist(product);
+        entityManager.persist(customer);
+        entityManager.persist(loacation);
+        entityManager.persist(sale);
 
         entityManager.getTransaction().commit();
         entityManager.close();
